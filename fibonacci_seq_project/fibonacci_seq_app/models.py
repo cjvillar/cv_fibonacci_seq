454e64c106e4a66cacf21b0dad4a1d7890f2ab13
Chris Villarreal <cjvillar@users.noreply.github.com>
2023-07-28 13:11:43 -0700

    added models and ran migration for fib app

--- fibonacci_seq_project/fibonacci_seq_app/models.py
+++ fibonacci_seq_project/fibonacci_seq_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+from django.db import models
+from django.core.validators import MinValueValidator
+
+class FibonacciNumber(models.Model):
+    #ensure only pos ints with MinValueValidator
+    n = models.PositiveIntegerField(validators=[MinValueValidator(0)])
+    fibonacci_sequence = models.TextField()
+
+    def __str__(self):
+        return f"Fib seq for n={self.n}"
