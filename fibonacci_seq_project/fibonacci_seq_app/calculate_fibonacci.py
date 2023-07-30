a33322eb0d53b9122bb33dfbcee54086f8cfc115
Chris Villarreal <cjvillar@users.noreply.github.com>
2023-07-30 10:13:49 -0700

    updated calculate_fibonacci.py

--- fibonacci_seq_project/fibonacci_seq_app/calculate_fibonacci.py
+++ fibonacci_seq_project/fibonacci_seq_app/calculate_fibonacci.py
@@ -2,40 +2,45 @@
 calculate_fibonacci calculates the fibonacci seq for all the terms up to the given nth term and saves n and fibonacci_sequence in a db.
 If the nth terms fibonacci_sequence is in the database it will retreive to avoid redundant calculations.
 
+If the databse is empty, calculate_fibonacci will initialize it with the base case data.
 """
 
 from .models import FibonacciNumber
 
 
+def set_base_case():
+    if not FibonacciNumber.objects.exists():
+        # set the base case if databse empty
+        FibonacciNumber.objects.create(n=0, fibonacci_sequence="0")
+        FibonacciNumber.objects.create(n=1, fibonacci_sequence="0,1")
+        return True
+    return False
+
+
 def calculate_fibonacci(n):
+    # check if base case set
+    if set_base_case():
+        pass
+
     # check if fib seq for n exists in db
     fibonacci_entry_seq = FibonacciNumber.objects.filter(n=n).first()
 
     if fibonacci_entry_seq:
         return fibonacci_entry_seq.fibonacci_sequence
 
-    # if fib seq for n does not exist, calc
-    fibonacci_sequence = []
+    # get latest seq from db to build the next seq.
+    last_seq = FibonacciNumber.objects.latest("n")
+    fibonacci_sequence = list(map(int, last_seq.fibonacci_sequence.split(",")))
+    num_0, num_1 = fibonacci_sequence[-2:]
 
-    if n == 0:
-        fibonacci_sequence = [
-            0
-        ]  # as per, https://www.mathsisfun.com/numbers/fibonacci-sequence.html
-    elif n == 1:
-        fibonacci_sequence = [0, 1]
-    else:
-        # calc fib seq using the existing seq for smaller n if exists
-        # **the code below was refined and debugged using ChatGPT**
-        previous_sequence_str = calculate_fibonacci(n - 1)
-        previous_sequence = list(map(int, previous_sequence_str.split(",")))
-        next_num = previous_sequence[-1] + previous_sequence[-2]
-        # cat two list
-        fibonacci_sequence = previous_sequence + [next_num]
+    # calculate nth sequence iteratively from the last two numbers in the last sequence
+    for i in range(len(fibonacci_sequence), n + 1):
+        next_num = num_0 + num_1
+        fibonacci_sequence.append(next_num)
+        num_0, num_1 = num_1, next_num
 
-    # store fib seq for n in db
-    fibonacci_sequence_str = ",".join(map(str, fibonacci_sequence))
-    FibonacciNumber.objects.get_or_create(
-        n=n, defaults={"fibonacci_sequence": fibonacci_sequence_str}
-    )
+        # store fib seq for n=i in db
+        fibonacci_sequence_str = ",".join(map(str, fibonacci_sequence))
+        FibonacciNumber.objects.create(n=i, fibonacci_sequence=fibonacci_sequence_str)
 
     return fibonacci_sequence_str
